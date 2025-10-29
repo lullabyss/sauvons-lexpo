ce378baf9f2eb3f5db1d10947474718616e99130
Claire Hache <75484695+lullabyss@users.noreply.github.com>
2025-10-29 14:56:00 +0100

    Create drag-and-drop.js

---
+++ javascript/drag-and-drop.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded", function () {
+	// Jeu en drag and drop
+	// Besoin d'une bliblothèque JS pour fonctionner.
+
+	const droppable = new Draggable.Droppable(
+		document.querySelectorAll(".container"),
+		{
+			//Ici, je détermine les classes
+			draggable: ".item",
+			dropzone: ".dropzone",
+		}
+	);
+
+	let droppableOrigin;
+
+	// --- Draggable events --- //
+	droppable.on("drag:start", (evt) => {
+		droppableOrigin = evt.originalSource.parentNode.dataset.dropzone;
+	});
+
+
+	//Les fonctions qui font que si tout est remplis, ça débloque un truc.
+	droppable.on("droppable:dropped", (evt) => {
+		if (droppableOrigin !== evt.dropzone.dataset.dropzone) {
+			evt.cancel();
+		}
+
+		setTimeout(function () {
+			areAllElementsPlaced();
+		}, 500);
+	});
+
+	
+	// Quand le tableau est au bon endroit :
+	function areAllElementsPlaced() {
+		let all_emplacement_are_occupied = true;
+
+		document
+			.querySelectorAll(".emplacement")
+			.forEach(function (emplacement) {
+				if (
+					emplacement.classList.contains("draggable-dropzone--occupied") ===
+					false
+				)
+					all_emplacement_are_occupied = false;
+			});
+		// Si le tableau est bon endroit, le bouton .reussite apparait.
+		if (all_emplacement_are_occupied === true) {
+			document.querySelector(".reussite")?.classList.add("clickable");
+		} else {
+			document.querySelector(".reussite")?.classList.remove("clickable");
+		}
+	}
+});
